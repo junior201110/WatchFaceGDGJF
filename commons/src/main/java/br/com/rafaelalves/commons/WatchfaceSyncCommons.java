94cb969555be2b8ce60dead9bbe9708eec15b820
Rafael Alves <fael.anjelus@gmail.com>
2015-05-28 11:19:10 -0300

    First commit

---
+++ commons/src/main/java/br/com/rafaelalves/commons/WatchfaceSyncCommons.java
@@ -0,0 +1,15 @@
+package br.com.rafaelalves.commons;
+
+/**
+ * Created by Rafael Alves Feliciano on 05/05/15.
+ **/
+public final class WatchfaceSyncCommons {
+
+    private WatchfaceSyncCommons() {
+
+    }
+
+    public static final String PATH = "/simple_watch_face_config";
+    public static final String KEY_BACKGROUND_COLOUR = "KEY_BACKGROUND_COLOUR";
+    public static final String KEY_DATE_TIME_COLOUR = "KEY_DATE_TIME_COLOUR";
+}
